1a520c0d3d6c43a74d7336863bf1a688d3a53de5
Festison <105289311+Festison@users.noreply.github.com>
2023-07-11 17:24:16 +0900

    Add files via upload

---
+++ C++/2주차/1. 함수의 오버로딩(07-10).cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+
+void PrintMenu()
+{
+	cout << "-------------" << endl;
+	cout << "---Default---" << endl;
+	cout << "-------------" << endl;
+	cout << endl;
+}
+
+void PrintMenu(int i)
+{
+	cout << "-------------" << endl;
+	cout << "----Menu" << i << "----" << endl;
+	cout << "-------------" << endl;
+	cout << endl;
+}
+
+void ShowInventory()
+{
+	int inventorySize = 10;
+
+	cout << "--------Inventory--------" << endl;
+
+	for (int i = 0; i < inventorySize; i++)
+	{
+		cout << i << "번쨰 인벤토리 아이템" << endl;
+	}
+
+	cout << "--------Inventory--------" << endl;
+	cout << endl;
+}
+
+void ShowInventory(int index)
+{
+	cout << index << "번째 인벤토리 아이템" << endl;
+	cout << endl;
+}
+
+void ShowInventory(float value)
+{
+	cout << "잘못된 데이터타입 임력됨" << endl;
+	cout << endl;
+}
+
+void ShowInventory(char ch)
+{
+	cout << "문자형 데이터 타입 입력" << endl;
+	cout << endl;
+}
+
+void ShowInventory(bool pro)
+{
+	cout << "참 또는 거짓" << endl;
+	cout << endl;
+}
+
+int main()
+{
+	// 함수 오버로딩 : 함수의 이름이 같아도 매개변수에 따라 다양한 결과가 나오도록 하는 기법
+	// 다형성
+
+	PrintMenu();
+	PrintMenu(2);
+	ShowInventory();
+	ShowInventory(8);
+	ShowInventory(3.54f);
+	ShowInventory('a');
+	ShowInventory(true);
+
+	return 0;
+}
---
+++ C++/2주차/2. 함수의 전방 선언(07-10).cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+// 전방 선언
+void Gugudan();
+void Gugudan(int dan);
+int Gugudan(int numA, int numB);
+
+int main()
+{
+	Gugudan();
+	Gugudan(5);
+	cout << Gugudan(5, 5) << endl;
+}
+
+void Gugudan(int dan)
+{
+	for (int i = 1; i < 10; i++)
+	{
+		cout << dan << " * " << i << " = " << dan * i << endl;
+	}
+
+	cout << endl;
+}
+
+void Gugudan()
+{
+	for (int i = 1; i < 10; i++)
+	{
+		Gugudan(i);
+	}
+
+	cout << endl;
+}
+
+int Gugudan(int numA, int numB)
+{
+	cout << numA << " * " << numB << " = ";
+
+	return numA * numB;
+}
+

ccd9b4ac508d946138a4a19875cd1f6a1e5fb469
Festison <105289311+Festison@users.noreply.github.com>
2023-08-22 21:25:56 +0900

    Add files via upload

---
+++ C#및 Unity/9주차/1. 객체의 회전 (08 -22).cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 1. 객체의 회전 : MonoBehaviour
+{ 
+    // 객체의 회전
+    // Rotate(회전하고자하는 축, 회전할 앵글값, Space.Self는 로컬 좌표계 (자신을 기준으로) , Space.World는 월드 좌표계 (세상을 기준으로))
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q)
+        {
+            transform.Rotate(Vector3.up, -1, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.Q)
+        {
+            transform.Rotate(Vector3.up, 1, Space.Self);
+        }
+    }
+}

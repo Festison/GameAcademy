28c58935174e360bd9411275484d60d8e6b1c216
Festison <105289311+Festison@users.noreply.github.com>
2023-11-21 09:06:55 +0900

    Add files via upload

---
+++ C#및 Unity/21주차/Score.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour, IPlayerInteractionable
+{
+    public void Interaction(PlayerScript player)
+    {
+        GameObject.FindObjectOfType<GameManager>().Score++;
+        Destroy(gameObject);
+    }
+}
---
+++ C#및 Unity/21주차/Wall.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour, IPlayerInteractionable
+{
+    public void Interaction(PlayerScript player)
+    {
+        GameObject.FindObjectOfType<GameManager>().GameOver();
+    }
+}
